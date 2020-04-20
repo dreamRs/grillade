905f10ca357a790d7192cec383cb8cfd80a87fda
pvictor <perrier.victor@gmail.com>
2020-04-20 20:02:01 +0200

    markdown experiments

--- R/markdown.R
+++ R/markdown.R
@@ -1,12 +1,15 @@
 
-html_grillade <- function(max_width = "1280px", ...) {
+html_grillade <- function(max_width = "100%", ...) {
   grlldep <- html_dependency_grillade()
   grlldep$head <- sprintf(
-    "<style>.main-container {max-width: %s;}</style>",
+    "<style>.main-container {max-width: %s !important;}</style>",
     htmltools::validateCssUnit(max_width)
   )
-  rmarkdown::html_document_base(
-    # theme = NULL,
+  grlldep$stylesheet <- c(
+    grlldep$stylesheet,
+    "grillade-rmd.css"
+  )
+  rmarkdown::html_document(
     extra_dependencies = list(grlldep),
     ...
   )
